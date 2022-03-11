f2f635a687f7f7e072fbf495bc65b9c6eb5bb814
Migueliot <migueliot@protonmail.com>
2022-03-12 00:14:13 +0300

    Add class Rectangle, changed class Point functions
    class Rectangle has two parameters:
    - class Point as a lowleft corner of rectangle
    - class Point as a upright corner of rectangle
    
    Changed function falls_in_rectangle from class Point so it would accept class Rectangle instance as a parameter.
    
    Also added comments to all the functions explaining what they do and removed not needed class Point constructor.

--- geom.py
+++ geom.py
@@ -3,14 +3,11 @@ import math
 # class Point contains x and y coordinates
 class Point:
 
-    def __init_(self):
-        self.x = 0
-        self.y = 0
-
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    # comparing if points have identical coordinates
     def compare(self, point):
         if self.x != point.x:
             check_x = False
@@ -27,18 +24,20 @@ class Point:
 
         return False
 
+    # showing coordinates of a point
     def show_coords(self):
         print('x:', self.x)
         print('y:', self.y)
 
-    def falls_in_rectangle(self, lowleft_t, upright_t):
-        # lowleft_t and upright_t are tuples with (x, y) values
-        if lowleft_t[0] < self.x < upright_t[0] \
-        and lowleft_t[1] < self.y < upright_t[1]:
+    # checking if a point is inside given rectangle instance or on its edge
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.lowleft.x <= self.x <= rectangle.upright.x \
+        and rectangle.lowleft.y <= self.y <= rectangle.upright.y:
             return True
         else:
             return False
 
+    # finding a distance from the given coordinates
     def distance_from_point(self, x, y):
         # basically finding a hypotenuse of a triangle created by two points
         side1 = self.x - x
@@ -49,6 +48,15 @@ class Point:
 
 
 
+class Rectangle:
+
+    # initialize a rectangle from two class Points instances
+    def __init__(self, lowleft, upright):
+        self.lowleft = lowleft
+        self.upright = upright
+
+
+
 # DEBUG
 point1 = Point(2, 2)
 print ('x:', point1.x)
@@ -56,3 +64,7 @@ print('y:', point1.y)
 
 point2 = Point(4, 3)
 print('distance:', point1.distance_from_point(point2.x, point2.y))
+
+rectanglex = Rectangle(point1, point2)
+point_inside = Point(3, 2)
+print('point_inside is in rectanglex?', point_inside.falls_in_rectangle(rectanglex))
