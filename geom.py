b609f51864e7250891e91bb10fcee32882fb381e
Migueliot1 <migueliot@protonmail.com>
2022-03-15 00:55:31 +0300

    Rename Rectangle's parameters.
    Since the class works well even if 2 points aren't exactly lowleft and
    upright, it's fine to rename them.
    
    Also added turtle.done() to main file so the created canvas shows after
    running the file.

--- geom.py
+++ geom.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 # class Point contains x and y coordinates
 class Point:
@@ -48,19 +49,19 @@ class Point:
 class Rectangle:
 
     # initialize a rectangle from two class Points instances
-    def __init__(self, lowleft, upright):
+    def __init__(self, point1, point2):
         
-        if type(lowleft) != Point and type(upright) != Point:
-            lowleft = Point(0, 0)
-            upright = Point(0, 0)
+        if type(point1) != Point and type(point2) != Point:
+            point1 = Point(0, 0)
+            point2 = Point(0, 0)
         
-        self.lowleft = lowleft
-        self.upright = upright
+        self.point1 = point1
+        self.point2 = point2
 
     # checking if an instance was created with bad parameters or not
     def is_defect(self):
 
-        if self.lowleft.compare(Point(0, 0)) and self.upright.compare(Point(0, 0)):
+        if self.point1.compare(Point(0, 0)) and self.point2.compare(Point(0, 0)):
             return True
         else:
             return False
@@ -71,32 +72,7 @@ class Rectangle:
         if self.is_defect():
             return None
         
-        return (self.upright.x - self.lowleft.x) * (self.upright.y - self.lowleft.y)
+        return (self.point2.x - self.point1.x) * (self.point2.y - self.point1.y)
 
 
 # DEBUG
-point1 = Point(2, 2)
-print ('x:', point1.x)
-print('y:', point1.y)
-
-point2 = Point(4, 3)
-print('distance:', point1.distance_from_point(point2.x, point2.y))
-
-rectanglex = Rectangle(point1, point2)
-point_inside = Point(3, 2)
-print('point_inside is in rectanglex?', point_inside.falls_in_rectangle(rectanglex))
-
-# trying sanity checks
-print(point1.compare(5))
-print('point_inside is in rectanglex?', point_inside.falls_in_rectangle(point2))
-
-rectangle_error = Rectangle(0, 0)
-print('rectangle_error lowleft x:', rectangle_error.lowleft.x)
-print('rectangle_error upright y:', rectangle_error.upright.y)
-print('is rectangle_error defect?', rectangle_error.is_defect())
-print('is rectanglex defect?', rectanglex.is_defect())
-
-print('area of rectanglex:', rectanglex.area())
-
-rectangle0 = Rectangle(Point(5, 10), Point(10, 15))
-print('area of rectangle0:', rectangle0.area())
--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import turtle
 
 # Create a canvas instance
 myTurtle = turtle.Turtle()
+
+turtle.done()
