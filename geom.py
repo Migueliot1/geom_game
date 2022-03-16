9ac6fa3ded75aa98edb1c140ac0c133247a68d5d
Migueliot1 <migueliot@protonmail.com>
2022-03-16 03:42:22 +0300

    Move all output into main.py

--- geom.py
+++ geom.py
@@ -105,32 +105,3 @@ class GuiPoint(Point):
         canvas.goto(self.x, self.y)
         canvas.pendown()
         canvas.dot(size, color)
-
-
-# Get point and an area from user
-user_x = float(input('Guess x: '))
-user_y = float(input('Guess y: '))
-user_area = float(input('Guess rectangle area: '))
-
-# Create a canvas
-myturtle = turtle.Turtle()
-
-# Create a point out of user's input and a randomly generated rectangle
-user_point = GuiPoint(user_x, user_y)
-rand_rect = GuiRectangle(Point(randint(0, 400), randint(0, 400)),
-                Point(randint(10, 400), randint(10, 400)))
-
-# Print out the game result
-msg = "Rectangle's coordinates: {x1}, {y1} and {x2}, {y2}."
-
-print('***')
-print(msg.format(x1=rand_rect.point1.x, y1=rand_rect.point1.y, x2=rand_rect.point2.x, y2=rand_rect.point2.y))
-print('Your point was inside rectangle:', user_point.falls_in_rectangle(rand_rect))
-print('Your area was off by:', rand_rect.area() -  user_area)
-print('***')
-
-# Draw rectangle and point
-rand_rect.draw(myturtle)
-user_point.draw(myturtle, 10)
-
-turtle.done()
